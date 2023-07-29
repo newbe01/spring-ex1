97daab07c147802f752557f910a345951439a589
newbe01 <pkwaa3@gmail.com>
2023-07-29 13:58:41 +0900

    member service test

---
+++ src/main/java/com/example/spring1/MemberApp.java
@@ -0,0 +1,23 @@
+package com.example.spring1;
+
+import com.example.spring1.member.Grade;
+import com.example.spring1.member.Member;
+import com.example.spring1.member.MemberService;
+import com.example.spring1.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+
+        Member member = new Member(1L, "MemberA", Grade.VIP);
+
+        memberService.join(member);
+
+        Member member1 = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("member1 = " + member1.getName());
+
+    }
+
+}
