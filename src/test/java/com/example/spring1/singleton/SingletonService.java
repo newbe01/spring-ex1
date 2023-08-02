6d7548925951deb399164b09994a229f4050d7c2
newbe01 <pkwaa3@gmail.com>
2023-08-02 13:38:16 +0900

    singletone

---
+++ src/test/java/com/example/spring1/singleton/SingletonService.java
@@ -0,0 +1,17 @@
+package com.example.spring1.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    private SingletonService() {}
+
+    public void logic() {
+        System.out.println("call singleton");
+    }
+
+}
