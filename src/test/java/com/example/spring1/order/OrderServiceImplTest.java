7ac6fb888e7391318d021ea56a41645aca5e5b57
newbe01 <pkwaa3@gmail.com>
2023-08-04 18:11:11 +0900

    constructor

---
+++ src/test/java/com/example/spring1/order/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.example.spring1.order;
+
+import com.example.spring1.discount.FixDiscountPolicy;
+import com.example.spring1.member.Grade;
+import com.example.spring1.member.Member;
+import com.example.spring1.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
