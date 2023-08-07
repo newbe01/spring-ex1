cd5a9638990ce83512c58961a99d2a0b5f86f4b3
newbe01 <pkwaa3@gmail.com>
2023-08-07 21:58:45 +0900

    web scope - proxy

--- src/main/java/com/example/spring1/web/LogDemoController.java
+++ src/main/java/com/example/spring1/web/LogDemoController.java
@@ -14,14 +14,17 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
 
     @ResponseBody
     @RequestMapping("log-demo")
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
