7d3a6bfe25b0170c6812e81863dd04720747eb87
newbe01 <pkwaa3@gmail.com>
2023-08-07 21:45:54 +0900

    web scope provider

--- src/main/java/com/example/spring1/web/LogDemoController.java
+++ src/main/java/com/example/spring1/web/LogDemoController.java
@@ -2,6 +2,7 @@ package com.example.spring1.web;
 
 import com.example.spring1.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,15 +14,18 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;    // request scope no bean
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
 
     @ResponseBody
     @RequestMapping("log-demo")
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testID");
         return "OK";
     }
--- src/main/java/com/example/spring1/web/LogDemoService.java
+++ src/main/java/com/example/spring1/web/LogDemoService.java
@@ -2,16 +2,18 @@ package com.example.spring1.web;
 
 import com.example.spring1.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
 
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
 
     }
